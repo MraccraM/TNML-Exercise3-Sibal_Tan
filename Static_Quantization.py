d456349bf45f187ed8495a5cc862580a55a589af
Sibal M <sibalmarcl@gmail.com>
2023-10-14 10:02:46 +0800

    tutorial complete

--- Static_Quantization.py
+++ Static_Quantization.py
@@ -254,6 +254,22 @@ def print_size_of_model(model):
     os.remove('temp.p')
     
 # 3. DEFINE DATASET AND DATA LOADERS
+# from tinyimagenet import TinyImageNet
+# from pathlib import Path
+# import logging
+
+# logging.basicConfig(level=logging.INFO)
+
+# split ="val"
+# dataset = TinyImageNet(Path("~/.torchvision/tinyimagenet/"),split=split)
+# n = len(dataset)
+# print(f"TinyImageNet, split {split}, has  {n} samples.")
+# n_samples = 5
+# print(f"Showing info of {n_samples} samples...")
+# for i in range(0,n,n//n_samples):
+#     image,klass = dataset[i]
+#     print(f"Sample of class {klass:3d}, image {image}, words {dataset.idx_to_words[klass]}")
+
 def prepare_data_loaders(data_path):
     normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                      std=[0.229, 0.224, 0.225])
@@ -285,9 +301,10 @@ def prepare_data_loaders(data_path):
 
     return data_loader, data_loader_test
 
-data_path = '~/.data/imagenet'
-saved_model_dir = 'data/'
-float_model_file = 'mobilenet_pretrained_float.pth'
+temp = os.path.abspath(os.getcwd())
+data_path = str(temp) + '\data_path\imagenet'
+saved_model_dir = str(temp)
+float_model_file = '\mobilenet_v2-b0353104.pth'
 scripted_float_model_file = 'mobilenet_quantization_scripted.pth'
 scripted_quantized_model_file = 'mobilenet_quantization_scripted_quantized.pth'
 
@@ -353,3 +370,104 @@ print_size_of_model(myModel)
 
 top1, top5 = evaluate(myModel, criterion, data_loader_test, neval_batches=num_eval_batches)
 print('Evaluation accuracy on %d images, %2.2f'%(num_eval_batches * eval_batch_size, top1.avg))
+
+per_channel_quantized_model = load_model(saved_model_dir + float_model_file)
+per_channel_quantized_model.eval()
+per_channel_quantized_model.fuse_model()
+# The old 'fbgemm' is still available but 'x86' is the recommended default.
+per_channel_quantized_model.qconfig = torch.ao.quantization.get_default_qconfig('x86')
+print(per_channel_quantized_model.qconfig)
+
+torch.ao.quantization.prepare(per_channel_quantized_model, inplace=True)
+evaluate(per_channel_quantized_model,criterion, data_loader, num_calibration_batches)
+torch.ao.quantization.convert(per_channel_quantized_model, inplace=True)
+top1, top5 = evaluate(per_channel_quantized_model, criterion, data_loader_test, neval_batches=num_eval_batches)
+print('Evaluation accuracy on %d images, %2.2f'%(num_eval_batches * eval_batch_size, top1.avg))
+torch.jit.save(torch.jit.script(per_channel_quantized_model), saved_model_dir + scripted_quantized_model_file)
+
+# 5. QUANTIZATION-AWARE TRAINING
+def train_one_epoch(model, criterion, optimizer, data_loader, device, ntrain_batches):
+    model.train()
+    top1 = AverageMeter('Acc@1', ':6.2f')
+    top5 = AverageMeter('Acc@5', ':6.2f')
+    avgloss = AverageMeter('Loss', '1.5f')
+
+    cnt = 0
+    for image, target in data_loader:
+        start_time = time.time()
+        print('.', end = '')
+        cnt += 1
+        image, target = image.to(device), target.to(device)
+        output = model(image)
+        loss = criterion(output, target)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        acc1, acc5 = accuracy(output, target, topk=(1, 5))
+        top1.update(acc1[0], image.size(0))
+        top5.update(acc5[0], image.size(0))
+        avgloss.update(loss, image.size(0))
+        if cnt >= ntrain_batches:
+            print('Loss', avgloss.avg)
+
+            print('Training: * Acc@1 {top1.avg:.3f} Acc@5 {top5.avg:.3f}'
+                  .format(top1=top1, top5=top5))
+            return
+
+    print('Full imagenet train set:  * Acc@1 {top1.global_avg:.3f} Acc@5 {top5.global_avg:.3f}'
+          .format(top1=top1, top5=top5))
+    return
+
+qat_model = load_model(saved_model_dir + float_model_file)
+qat_model.eval()
+qat_model.fuse_model()
+
+optimizer = torch.optim.SGD(qat_model.parameters(), lr = 0.0001)
+# The old 'fbgemm' is still available but 'x86' is the recommended default.
+qat_model.qconfig = torch.ao.quantization.get_default_qat_qconfig('x86')
+
+qat_model.train()
+torch.ao.quantization.prepare_qat(qat_model, inplace=True)
+print('Inverted Residual Block: After preparation for QAT, note fake-quantization modules \n',qat_model.features[1].conv)
+
+num_train_batches = 20
+
+# QAT takes time and one needs to train over a few epochs.
+# Train and check accuracy after each epoch
+for nepoch in range(8):
+    train_one_epoch(qat_model, criterion, optimizer, data_loader, torch.device('cpu'), num_train_batches)
+    if nepoch > 3:
+        # Freeze quantizer parameters
+        qat_model.apply(torch.ao.quantization.disable_observer)
+    if nepoch > 2:
+        # Freeze batch norm mean and variance estimates
+        qat_model.apply(torch.nn.intrinsic.qat.freeze_bn_stats)
+
+    # Check the accuracy after each epoch
+    quantized_model = torch.ao.quantization.convert(qat_model.eval(), inplace=False)
+    quantized_model.eval()
+    top1, top5 = evaluate(quantized_model,criterion, data_loader_test, neval_batches=num_eval_batches)
+    print('Epoch %d :Evaluation accuracy on %d images, %2.2f'%(nepoch, num_eval_batches * eval_batch_size, top1.avg))
+    
+def run_benchmark(model_file, img_loader):
+    elapsed = 0
+    model = torch.jit.load(model_file)
+    model.eval()
+    num_batches = 5
+    # Run the scripted model on a few batches of images
+    for i, (images, target) in enumerate(img_loader):
+        if i < num_batches:
+            start = time.time()
+            output = model(images)
+            end = time.time()
+            elapsed = elapsed + (end-start)
+        else:
+            break
+    num_images = images.size()[0] * num_batches
+
+    print('Elapsed time: %3.0f ms' % (elapsed/num_images*1000))
+    return elapsed
+
+run_benchmark(saved_model_dir + scripted_float_model_file, data_loader_test)
+
+run_benchmark(saved_model_dir + scripted_quantized_model_file, data_loader_test)
